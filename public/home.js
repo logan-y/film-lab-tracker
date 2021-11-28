92c4ba8da040a505a51573e145785437e69097bd
Logan Young <logany@att.net>
2021-11-28 11:00:43 -0600

    five timers set up front end only

--- public/home.js
+++ public/home.js
@@ -1,1 +1,7 @@
-//changesssss
+import Timer from "./timer.js"
+
+new Timer(document.getElementById("rinse"), " Rinse Timer");
+new Timer(document.getElementById("dev"), " Developer Timer");
+new Timer(document.getElementById("fixer"), " Fixer Timer");
+new Timer(document.getElementById("soak"), " Soak Timer");
+new Timer(document.getElementById("stbl"), " Stabilizer Timer");
